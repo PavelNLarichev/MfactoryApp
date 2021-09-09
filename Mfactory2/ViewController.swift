df6517c679ab3e31a854dcd31168e1e04564b540
PavelLarichev <me@plarichev.ru>
2021-09-09 08:49:06 +0300

    Initial Commit

---
+++ Mfactory2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mfactory2
+//
+//  Created by Павел Ларичев on 09.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
