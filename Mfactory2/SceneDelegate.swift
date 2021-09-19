20a03a1c755ee315c9c34c58879110f38aa4dbc5
PavelLarichev <me@plarichev.ru>
2021-09-19 19:37:00 +0300

    add NavigationController

--- Mfactory2/SceneDelegate.swift
+++ Mfactory2/SceneDelegate.swift
@@ -18,17 +18,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-      
         
+        
+        
+//        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+//        window?.windowScene = windowScene
+//        window?.rootViewController = mainScreenVC()
+//       window?.makeKeyAndVisible()
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        
         window?.windowScene = windowScene
-
-     window?.rootViewController = mainScreenVC()
-
-//     window?.rootViewController = rootScreen()
-        window?.makeKeyAndVisible()
+              let rootVC = rootScreen()
+                let rootNC = UINavigationController(rootViewController: rootVC)
+                window?.rootViewController = rootNC
+                window?.makeKeyAndVisible()
+        
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
